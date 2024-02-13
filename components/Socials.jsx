357a21cb0f573e804bccf84f013ab7b008652179
Nickolas <work35955@gmail.com>
2024-02-12 17:48:32 -0800

    change address

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -14,7 +14,7 @@ export const socialData = [
   },
   {
     name: "Github",
-    link: "https://github.com/sanidhyy/modern-portfolio",
+    link: "https://github.com/Nicko57",
     Icon: RiGithubLine,
   },
 ];
